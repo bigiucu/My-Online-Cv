5e9e2924ed94bcd432abbfc0eda7ececc66d94a6
bigiucu <cristea1989_ali@yahoo.com>
2019-12-13 19:56:08 +0200

    Contact page

--- My Cv/js/contact.js
+++ My Cv/js/contact.js
@@ -1,21 +1,44 @@
 
 function shoe(x){
   let div = document.getElementById("show_div");
-  div.innerHTML=`<i class="fa fa-exclamation-triangle"></i> ${x}`
+  div.innerHTML=`<div class="show_message"><i class="fa fa-exclamation-triangle"></i> ${x}<div>`
 }
 
-function checkForm(event) {
+function checkForm(){
+    var emailID = document.sentMessage.email1.value;
+    atpos = emailID.indexOf("@");
+    dotpos = emailID.lastIndexOf(".");
+    let default_text=document.getElementById("default_text")
+    if( document.sentMessage.username.value == "" ) {
+        shoe("Please provide your name!");
+        document.sentMessage.username.focus() ;
+        return false;
+     }
+     if( document.sentMessage.email1.value == "" || (atpos < 1 || ( dotpos - atpos < 2 ))) {
+        shoe("Please provide your Email!");
+        document.sentMessage.email1.focus() ;
+        return false;
+     }
+     if( document.sentMessage.phone1.value.length<10 ||document.sentMessage.phone1.value.length>10 || isNaN(document.sentMessage.phone1.value) ) {
+        shoe("Please provide a correct phone number");
+        document.sentMessage.phone1.focus() ;
+        return false;
+    }
+     if( document.sentMessage.message1.value < "10" ) {
+        shoe("Please leave a message!");
+        return false;
+     }
+     default_text.innerHTML=`<i class="fas fa-check color"></i><span> Your message was received!<span>`;
+     document.sentMessage.username.value="";
+     document.sentMessage.email1.value="";
+     document.sentMessage.phone1.value="";
+     document.sentMessage.message1.value="";
+     remove.shoe("");
+     return( true );
+}
+
+function addPost(event){
     event.preventDefault();
-    let username = document.getElementById("name").value;
-    let email = document.getElementById("email").value;
-    let phone = document.getElementById("phone").value;
-    let reg_email="/\S+@\S+\.\S+/";
-    let messageform = document.getElementById("message");
-}
-
-
-function addPost(){
-    checkForm(event);
     let name=document.getElementById("name").value;
     let email=document.getElementById("email").value;
     let phone=document.getElementById("phone").value;
@@ -45,7 +68,6 @@ function addPost(){
         `<p class="obj_form"><i class="far fa-envelope color-env"></i> Multumesc pentru mesaj ! </p><p><span class="data_form"> ${data.name} </span> , o sa iti raspund in cel mai scurt timp pe adresa <span class="data_form"> ${data.email}</span> sau la numarul de telefon <span class="data_form">${data.phone}</span>.
          Mesajul ${data.message} a fost primit!`)
         .catch(err => console.log('Error message:' ,  err.statusText))
-}
-
-document.getElementById("contactForm").addEventListener("submit",checkForm);
-document.getElementById("contactForm").addEventListener("input", addPost);
+    }
+document.getElementById("contactForm").addEventListener("submit", addPost);
+document.getElementById("contactForm").addEventListener("submit", checkForm);
